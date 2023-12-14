4ee02c90e74ba267906b2521a4dfe8be0d672523
Othman Ali <73076057+Othmanali02@users.noreply.github.com>
2023-12-14 15:21:15 +0200

    Initiated the application

---
+++ app/src/main/java/com/example/eventplanningapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.eventplanningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Event planner app
+        setContentView(R.layout.activity_main);
+    }
+}
